ff8c8ebc75f294f40de6d0e6b3099e0cf93e3cea
RamisettyNaveen <137636617+RamisettyNaveen@users.noreply.github.com>
2024-03-23 21:48:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+
+resource "aws_instance" "sample_machine"{
+    ami = "ami-0a1b648e2cd533174"
+    instance_type = "t2.micro"
+        tags = {Name = "HTTPS SERVER"}
+
